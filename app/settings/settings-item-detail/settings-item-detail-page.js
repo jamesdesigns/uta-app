83daa2785aa98c9006f19cafe563872c7622e604
James Hooper <jameshooperdesigns@gmail.com>
2019-05-08 22:15:20 -0600

    Edit the settings page

---
+++ app/settings/settings-item-detail/settings-item-detail-page.js
@@ -0,0 +1,15 @@
+function onNavigatingTo(args) {
+    const page = args.object;
+
+    page.bindingContext = args.context;
+}
+
+function onBackButtonTap(args) {
+    const view = args.object;
+    const page = view.page;
+
+    page.frame.goBack();
+}
+
+exports.onNavigatingTo = onNavigatingTo;
+exports.onBackButtonTap = onBackButtonTap;
